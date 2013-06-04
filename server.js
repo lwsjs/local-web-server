9d118e479843e4da320d6bff859aea5bedd42010
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-06-04 13:51:32 +0100

    port cmd line arg

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ var app = connect()
     .use(connect.logger('dev'))
     .use(connect.static(process.cwd()))
     .use(connect.directory(process.cwd()))
-    .listen(8000);
+    .listen(process.argv[2] || 8000);
