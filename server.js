a66163bd2d1bf031d40815228d7d6d346bec00b3
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-06-11 14:31:43 +0100

    'listening' text

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node 
 var connect = require('connect');
 
-var app = connect()
+var port = process.argv[2] || 8000;
+connect()
     .use(connect.logger('dev'))
     .use(connect.static(process.cwd()))
     .use(connect.directory(process.cwd()))
-    .listen(process.argv[2] || 8000);
+    .listen();
+    
+console.log("listening on port " + port);
