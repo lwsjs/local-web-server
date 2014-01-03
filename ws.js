b3bb5241228506ecb9465752a576be4fe5cb6823
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-01-03 18:04:44 +0000

    updated deps, better error handling, compression

--- ws.js
+++ ws.js
@@ -1,17 +1,28 @@
 #!/usr/bin/env node 
-var connect = require('connect'),
+var connect = require("connect"),
+    http = require("http"),
     Thing = require("nature").Thing;
 
-var usage = "usage: ws [--port|-p <port>] [--log-format|-p dev|default|short|tiny]";
+function red(txt){ return "\x1b[31m" + txt + "\x1b[0m"; }
+function green(txt){ return "\x1b[32m" + txt + "\x1b[0m"; }
+function halt(message){
+    console.log(red("Error ") + message);
+    console.log(usage);
+    process.exit(1);
+}
+
+var usage = "usage: ws [--port|-p <port>] [--log-format|-f dev|default|short|tiny]";
 var options = new Thing()
     .define({ name: "port", alias: "p", type: "number", defaultOption: true, default: 8000 })
     .define({ name: "log-format", alias: "f", type: "string", default: "dev" })
     .define({ name: "help", alias: "h", type: "boolean" })
+    .on("error", function(err){
+        halt(err.message);
+    })
     .set(process.argv);
 
 if (!options.valid){
-    console.log(usage);
-    throw new Error(options.validationMessages);
+    halt(options.validationMessages)
 
 } else if (options.help){
     console.log(usage);
@@ -26,10 +37,20 @@ if (!options.valid){
                 .replace("GMT", "").replace(" (BST)", "");
     });
 
-    connect()
+    var app = connect()
         .use(connect.logger(options["log-format"]))
+        .use(connect.compress())
         .use(connect.static(process.cwd()))
-        .use(connect.directory(process.cwd()))
+        .use(connect.directory(process.cwd(), { icons: true }));
+        
+    http.createServer(app)
+        .on("error", function(err){
+            if (err.code === "EADDRINUSE"){
+                halt("port " + options.port + " is already is use")
+            } else {
+                halt(err.message);
+            }
+        })
         .listen(options.port);
 
     process.stderr.write("serving at http://localhost:" + options.port + "\n");
