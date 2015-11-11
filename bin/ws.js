b19545d3fcc938bde3dcc797960984bb30d14639
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-11 17:53:01 +0000

    cli rewrite

--- bin/ws.js
+++ bin/ws.js
@@ -2,16 +2,68 @@
 'use strict'
 const localWebServer = require('../')
 const streamLogStats = require('stream-log-stats')
+const commandLineArgs = require('command-line-args')
+const ansi = require('ansi-escape-sequences')
+const cliOptions = require('../lib/cli-options')
+const loadConfig = require('config-master')
+const path = require('path')
 
-const options = {
-  static: { root: '.' },
-  serveIndex: { path: '.' },
-  logger: { format: 'common', options: {
-    stream: streamLogStats({ refreshRate: 100 })}
-  }
+const options = {}
+
+/* parse command line args */
+const cli = commandLineArgs(cliOptions.definitions)
+const usage = cli.getUsage(cliOptions.usageData)
+
+try {
+  options.cli = cli.parse()
+} catch (err) {
+  halt(err.message)
 }
 
-localWebServer(options)
-  .listen(8000, () => {
-    console.log(`listening`)
-  })
+options.stored = loadConfig('local-web-server')
+options.builtIn = {
+  port: 8000,
+  directory: process.cwd(),
+  'refresh-rate': 500,
+  mime: {}
+}
+
+/* override built-in defaults with stored config and then command line args */
+options.cli.server = Object.assign(options.builtIn, options.stored, options.cli.server)
+
+if (options.cli.misc.help) return console.log(usage)
+if (options.cli.misc.config) return console.log(JSON.stringify(options.stored, null, '  '))
+
+let log = {
+  format: options.cli.server['log-format']
+}
+
+if (log.format === 'none'){
+  log.format = undefined
+} else if (log.format){
+  log.stream = process.stdout
+} else {
+  log.format = 'common'
+  log.stream = streamLogStats({ refreshRate: 100 })
+}
+
+localWebServer({
+  static: { root: options.cli.server.directory },
+  serveIndex: { path: options.cli.server.directory, options: { icons: true } },
+  logger: { format: log.format, options: { stream: log.stream } }
+}).listen(options.cli.server.port, serverUp)
+
+function halt (message) {
+  console.log(ansi.format(`Error: ${message}`, 'red'))
+  console.log(usage)
+  process.exit(1)
+}
+
+function serverUp () {
+  /* write launch information to stderr (stdout is reserved for web log output) */
+  if (path.resolve(options.cli.server.directory) === process.cwd()) {
+    console.error(ansi.format(`serving at [underline]{http://localhost:${options.cli.server.port}}`))
+  } else {
+    console.error(ansi.format(`serving [underline]{options.cli.server.directory} at [underline]{http://localhost:${options.cli.server.port}}`))
+  }
+}
