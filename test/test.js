d486b7a93a6b0796e53dbcb3eca14c73f1af7ea2
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-19 13:40:30 +0000

    fixed issue with rewrite 'from' urls containing ports

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ function launchServer (app, options) {
   options = options || {}
   const path = `http://localhost:8100${options.path || '/'}`
   const server = http.createServer(app.callback())
-  return server.listen(8100, () => {
+  return server.listen(options.port || 8100, () => {
     const req = request(path, options.reqOptions)
     if (options.onSuccess) req.then(options.onSuccess)
     if (!options.leaveOpen) req.then(() => server.close())
@@ -163,4 +163,29 @@ test('rewrite: proxy', function(t){
     t.strictEqual(response.res.statusCode, 200)
     t.ok(/db_name/.test(response.data))
   }})
+})
+
+test('rewrite: proxy with port', function(t){
+  t.plan(2)
+  const one = localWebServer({
+    log: { format: 'none' },
+    static: { root: __dirname + '/fixture/one' }
+  })
+  const two = localWebServer({
+    log: { format: 'none' },
+    static: { root: __dirname + '/fixture/spa' },
+    rewrite: [ { from: '/test/*', to: 'http://localhost:9000/$1'} ]
+  })
+  const server1 = http.createServer(one.callback())
+  const server2 = http.createServer(two.callback())
+  server1.listen(9000, () => {
+     server2.listen(8100, () => {
+       request('http://localhost:8100/test/file.txt').then(response => {
+         t.strictEqual(response.res.statusCode, 200)
+         t.ok(/one/.test(response.data))
+         server1.close()
+         server2.close()
+       })
+     })
+  })
 })
