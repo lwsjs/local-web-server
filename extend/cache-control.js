027c25f53fb154811fc2d4ec05037ff3ed1ff0c6
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-06-09 22:54:57 +0100

    refactor

--- extend/cache-control.js
+++ extend/cache-control.js
@@ -1,24 +1,16 @@
 'use strict'
-const Cli = require('../cli')
+const Cli = require('../')
 const cacheControl = require('koa-cache-control')
 const cliData = require('../lib/cli-data')
 
-cliData.push({ name: 'black' })
+cliData.optionDefinitions.push({ name: 'maxage', group: 'misc' })
 
-const ws = new Cli({
-  'no-cache': true,
-  log: { format: 'dev' }
-})
-
-ws.middleware.splice(
-  ws.middleware.findIndex(m => m.name === 'mime-type'),
-  1,
-  {
-    name: 'cache-control',
-    create: convert(cacheControl({
-      maxAge: 15
-    }))
-  }
-)
-
+const ws = new Cli()
+ws.middleware
+  .addLogging('dev')
+  .add(cacheControl({
+    maxAge: 15
+  }))
+  .addStatic()
+  .addIndex()
 ws.listen()
--- extend/live-reload.js
+++ extend/live-reload.js
@@ -1,14 +1,10 @@
 'use strict'
-const Koa = require('koa')
-const localWebServer = require('../')
+const Cli = require('../')
 const liveReload = require('koa-livereload')
-const convert = require('koa-convert')
 
-const app = new Koa()
-const ws = localWebServer({
-  log: { format: 'dev' }
-})
-
-app.use(liveReload())
-app.use(ws)
-app.listen(8000)
+const ws = new Cli()
+ws.middleware
+  .addLogging('dev')
+  .add(liveReload())
+  .addStatic()
+ws.listen(8000)
