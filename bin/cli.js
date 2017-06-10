b7289201e1ca4bbd73470798a58f0d8ebbab1b48
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-06-10 21:16:08 +0100

    refactor bin run

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,2 @@
 #!/usr/bin/env node
-'use strict'
-const LocalWebServer = require('../')
-const localWebServer = new LocalWebServer()
-try {
-  localWebServer.start()
-} catch (err) {
-  if (err.code === 'MODULE_NOT_FOUND') {
-    console.error(err.message)
-    console.error(require('util').inspect(err.attempted, { depth: 6, colors: true }))
-  } else {
-    console.error(err.stack)
-  }
-}
+require('../').run()
