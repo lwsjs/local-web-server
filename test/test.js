008d9dbfa5231e53ec9ef6f88fcba1b9b6d093d0
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-06-25 19:21:38 +0100

    upgrade deps.. test

--- test/test.js
+++ test/test.js
@@ -8,13 +8,12 @@ const runner = new TestRunner()
 
 runner.test('basic', async function () {
   const port = 9000 + this.index
-  const localWebServer = new LocalWebServer({
+  const localWebServer = new LocalWebServer()
+  const server = localWebServer.create({
     port: port,
-    directory: 'test/fixture',
-    logFormat: 'none'
+    directory: 'test/fixture'
   })
-  localWebServer.launch()
   const response = await request(`http://localhost:${port}/one.txt`)
-  localWebServer.server.close()
+  server.close()
   a.strictEqual(response.data.toString(), 'one\n')
 })
