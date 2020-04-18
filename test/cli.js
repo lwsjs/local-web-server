90dfc6fbd983f96e54ada14ab4e9f4dc45aa1f3d
andyemre-com <emreagarta@gmail.com>
2020-04-18 14:50:45 +0300

    Update cli.js

--- test/cli.js
+++ test/cli.js
@@ -1,58 +1,43 @@
 const Tom = require('test-runner').Tom
-const a = require('assert')
+const a = require('assert').strict
 const WsCli = require('../lib/cli-app')
 const fetch = require('node-fetch')
 
-const tom = module.exports = new Tom('cli')
+const tom = module.exports = new Tom({ maxConcurrency: 1 })
 
-tom.test('cli.run', async function () {
+tom.test('simple', async function () {
   const port = 7500 + this.index
-  const origArgv = process.argv.slice()
-  process.argv = [ 'node', 'something', '--port', `${port}` ]
   const cli = new WsCli({ logError: function () {} })
-  const server = cli.start()
-  process.argv = origArgv
+  const server = cli.start([ '--port', `${port}` ])
   const response = await fetch(`http://127.0.0.1:${port}/package.json`)
   server.close()
-  a.strictEqual(response.status, 200)
+  a.equal(response.status, 200)
 })
 
-tom.test('cli.run: bad option', async function () {
-  const origArgv = process.argv.slice()
-  process.argv = [ 'node', 'something', '--should-fail' ]
+tom.test('bad option', async function () {
   const exitCode = process.exitCode
   const cli = new WsCli({ logError: function () {} })
-  const server = cli.start()
+  const server = cli.start([ '--should-fail' ])
   if (!exitCode) process.exitCode = 0
-  process.argv = origArgv
-  a.strictEqual(server, undefined)
+  a.equal(server, undefined)
 })
 
-tom.test('cli.run: --help', async function () {
-  const origArgv = process.argv.slice()
-  process.argv = [ 'node', 'something', '--help' ]
+tom.test('--help', async function () {
   const cli = new WsCli({ log: function () {} })
-  cli.start()
-  process.argv = origArgv
+  cli.start([ '--help' ])
 })
 
-tom.test('cli.run: --version', async function () {
-  const origArgv = process.argv.slice()
-  process.argv = [ 'node', 'something', '--version' ]
+tom.test('--version', async function () {
   let logMsg = ''
   const cli = new WsCli({ log: function (msg) { logMsg = msg } })
-  cli.start()
+  cli.start([ '--version' ])
   const pkg = require('../package.json')
-  a.strictEqual(logMsg.trim(), pkg.version)
-  process.argv = origArgv
+  a.equal(logMsg.trim(), pkg.version)
 })
 
-tom.test('cli.run: default-stack', async function () {
-  const origArgv = process.argv.slice()
-  process.argv = [ 'node', 'something', '--default-stack' ]
+tom.test('default-stack', async function () {
   let logMsg = ''
   const cli = new WsCli({ log: function (msg) { logMsg = msg } })
-  cli.start()
+  cli.start([ '--default-stack' ])
   a.ok(/lws-static/.test(logMsg))
-  process.argv = origArgv
 })
